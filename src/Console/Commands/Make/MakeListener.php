459b7388b804e70a6c37d91e43fea16af9ff8fe5
RT Filefabrik <rt@filefabriik.com>
2024-08-14 10:43:43 +0200

    v1.0.5 MakeListener with option test

--- src/Console/Commands/Make/MakeListener.php
+++ src/Console/Commands/Make/MakeListener.php
@@ -3,6 +3,7 @@
 namespace Filefabrik\Paxsy\Console\Commands\Make;
 
 use Filefabrik\Bootraiser\Support\Str\Namespacering;
+use Filefabrik\Paxsy\Console\Commands\Admin\TraitOptions;
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Foundation\Console\ListenerMakeCommand;
 use Illuminate\Support\Str;
@@ -10,37 +11,42 @@ use Illuminate\Support\Str;
 // todo bug, will not generate the Event and not showing the "Create Event" prompt
 class MakeListener extends ListenerMakeCommand
 {
-	use TraitModularize;
+    use TraitModularize, TraitOptions;
+    use TraitCreatesMatchingTest;
+    use TraitCallDelegation;
 
-	protected function buildClass($name): array|string
-	{
-		$event = $this->option('event');
+    protected function buildClass($name): array|string
+    {
+        $event = $this->option('event');
 
-		if ($event && $this->isInNamespace($name)) {
-			$stub = str_replace(
-				['DummyEvent', '{{ event }}'],
-				class_basename($event),
-				GeneratorCommand::buildClass($name),
-			);
+        if ($event && $this->isInNamespace($name)) {
+            $stub = str_replace(
+                ['DummyEvent', '{{ event }}'],
+                class_basename($event),
+                GeneratorCommand::buildClass($name),
+            );
 
-			return str_replace(
-				['DummyFullEvent', '{{ eventNamespace }}'],
-				Namespacering::trim($event),
-				$stub,
-			);
-		}
+            return str_replace(
+                ['DummyFullEvent', '{{ eventNamespace }}'],
+                Namespacering::trim($event),
+                $stub,
+            );
+        }
 
-		return parent::buildClass($name);
-	}
+        return parent::buildClass($name);
+    }
 
-	/**
-	 * Todo Move to package Helper Methods
-	 * @param $name
-	 *
-	 * @return bool
-	 */
-	protected function isInNamespace($name): bool
-	{
-		return $this->package() && Str::startsWith($name, $this->package()->srcPackageNamespace());
-	}
+    /**
+     * Todo Move to package Helper Methods
+     *
+     * @param $name
+     *
+     * @return bool
+     */
+    protected function isInNamespace($name): bool
+    {
+        return $this->package() && Str::startsWith($name,
+                                                   $this->package()
+                                                        ->srcPackageNamespace());
+    }
 }
