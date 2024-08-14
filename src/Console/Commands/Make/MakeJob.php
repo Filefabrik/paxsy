37cde690a84f59638ccb99b075f5567f8b856a34
RT Filefabrik <rt@filefabriik.com>
2024-08-14 10:03:01 +0200

    v1.0.5 MakeJob with option test

--- src/Console/Commands/Make/MakeJob.php
+++ src/Console/Commands/Make/MakeJob.php
@@ -2,10 +2,14 @@
 
 namespace Filefabrik\Paxsy\Console\Commands\Make;
 
+use Filefabrik\Paxsy\Console\Commands\Admin\TraitOptions;
 use Illuminate\Foundation\Console\JobMakeCommand;
 
 // @codeCoverageIgnore
 class MakeJob extends JobMakeCommand
 {
 	use TraitModularize;
+	use TraitOptions;
+	use TraitCreatesMatchingTest;
+	use TraitCallDelegation;
 }
--- tests/Unit/Console/Commands/Make/MakeJobTest.php
+++ tests/Unit/Console/Commands/Make/MakeJobTest.php
@@ -31,3 +31,51 @@ it(
 		expect(file_get_contents($expected_full_path))->toContain(...$expected_substrings);
 	},
 );
+
+it(
+	'with php unit',
+	function() {
+		makePackageByArtisanCommand($this);
+		withGuiInteractions();
+
+		$command                = 'make:job';
+		$arguments              = [/*'name' => 'TestComponent'*/];
+		$expected_relative_path = 'src/Jobs/JannisJobblessJob.php';
+		$expected_substrings    = [
+			'namespace '.DefaultPackageNames::namespacyfy('Job'),
+			'class JannisJobblessJob',
+		];
+
+		//	makeComponentInPackage($this, $command, $arguments);
+
+		$packageName = defaultTestPackage()
+			->getPackageName()
+		;
+		$this->artisan(
+			$command,
+			array_merge(
+				[
+					'--package' => $packageName,
+				],
+				$arguments,
+			),
+		)
+			 ->assertExitCode(0)
+			 ->expectsQuestion('Options for MakeJob?', ['test'])
+			 ->expectsQuestion('What should the job be named?', 'JannisJobblessJob')
+			// todo check class content
+			// todo ...maybe bullshit to render feature test
+			 ->expectsQuestion('Options for MakeTest?', ['phpunit'])
+		;
+
+		$expected_full_path = DefaultPackageNames::VendorPackageComponentPath($expected_relative_path);
+
+		checkComponentFilesAndDirectories($expected_full_path);
+		expect(file_get_contents($expected_full_path))->toContain(...$expected_substrings);
+
+		// test output
+		$expected_full_path = DefaultPackageNames::VendorPackageComponentPath('tests/Feature/Jobs/JannisJobblessJobTest.php');
+
+		checkComponentFilesAndDirectories($expected_full_path);
+	}
+);
