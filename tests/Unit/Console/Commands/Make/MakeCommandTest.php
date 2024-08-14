44431f9ab00817fbbcdb872964df6a6c1f4651c8
RT Filefabrik <rt@filefabriik.com>
2024-08-14 09:57:10 +0200

    v1.0.5 MakeCommand with option test

--- tests/Unit/Console/Commands/Make/MakeCommandTest.php
+++ tests/Unit/Console/Commands/Make/MakeCommandTest.php
@@ -34,3 +34,50 @@ it(
 		expect(file_get_contents($expected_full_path))->toContain(...$expected_substrings);
 	},
 );
+it(
+	'command with test',
+	function() {
+		makePackageByArtisanCommand($this);
+		withGuiInteractions();
+
+		$command                = 'make:command';
+		$arguments              = [/*'name' => 'TestComponent'*/];
+		$expected_relative_path = 'src/Console/Commands/FlightCommand.php';
+		$expected_substrings    = [
+			'namespace '.DefaultPackageNames::namespacyfy('Console\Commands'),
+			'class FlightCommand',
+		];
+
+		//	makeComponentInPackage($this, $command, $arguments);
+
+		$packageName = defaultTestPackage()
+			->getPackageName()
+		;
+		$this->artisan(
+			$command,
+			array_merge(
+				[
+					'--package' => $packageName,
+				],
+				$arguments,
+			),
+		)
+			 ->assertExitCode(0)
+			 ->expectsQuestion('Options for MakeCommand?', ['test'])
+			 ->expectsQuestion('What should the console command be named?', 'FlightCommand')
+			// todo check class content
+			// todo ...maybe bullshit to render feature test
+			 ->expectsQuestion('Options for MakeTest?', ['phpunit'])
+		;
+
+		$expected_full_path = DefaultPackageNames::VendorPackageComponentPath($expected_relative_path);
+
+		checkComponentFilesAndDirectories($expected_full_path);
+		expect(file_get_contents($expected_full_path))->toContain(...$expected_substrings);
+
+		// test output
+		$expected_full_path = DefaultPackageNames::VendorPackageComponentPath('tests/Feature/Console/Commands/FlightCommandTest.php');
+
+		checkComponentFilesAndDirectories($expected_full_path);
+	}
+);
