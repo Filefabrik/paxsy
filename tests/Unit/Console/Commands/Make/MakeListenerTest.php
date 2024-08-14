0ba9a77c9dfa7a32c947d9d342183eefd2982f51
RT Filefabrik <rt@filefabriik.com>
2024-08-14 10:43:15 +0200

    v1.0.5 MakeListener with option test

--- tests/Unit/Console/Commands/Make/MakeListenerTest.php
+++ tests/Unit/Console/Commands/Make/MakeListenerTest.php
@@ -51,3 +51,50 @@ it(
 		expect(file_get_contents($expected_full_path))->toContain(...$expected_substrings);
 	},
 );
+it(
+    'with php unit',
+    function() {
+        makePackageByArtisanCommand($this);
+        withGuiInteractions();
+
+        $command                = 'make:listener';
+        $arguments              = [/*'name' => 'TestComponent'*/];
+        $expected_relative_path = 'src/Listeners/Leasetainer.php';
+        $expected_substrings    = [
+            'namespace '.DefaultPackageNames::namespacyfy('Listeners'),
+            'class Leasetainer',
+        ];
+
+        //	makeComponentInPackage($this, $command, $arguments);
+
+        $packageName = defaultTestPackage()
+            ->getPackageName()
+        ;
+        $this->artisan(
+            $command,
+            array_merge(
+                [
+                    '--package' => $packageName,
+                ],
+                $arguments,
+            ),
+        )
+             ->assertExitCode(0)
+             ->expectsQuestion('Options for MakeListener?', ['test'])
+             ->expectsQuestion('What should the listener be named?', 'Leasetainer')
+            // todo check class content
+            // todo ...maybe bullshit to render feature test
+             ->expectsQuestion('Options for MakeTest?', ['phpunit'])
+        ;
+
+        $expected_full_path = DefaultPackageNames::VendorPackageComponentPath($expected_relative_path);
+
+        checkComponentFilesAndDirectories($expected_full_path);
+        expect(file_get_contents($expected_full_path))->toContain(...$expected_substrings);
+
+        // test output
+        $expected_full_path = DefaultPackageNames::VendorPackageComponentPath('tests/Feature/Listeners/LeasetainerTest.php');
+
+        checkComponentFilesAndDirectories($expected_full_path);
+    }
+);
