14000a33a79bbc74665619512fb5e9175f8dfa11
RT Filefabrik <rt@filefabriik.com>
2024-08-17 23:08:08 +0200

    v1.0.6
    Codeclimate

--- src/Console/Commands/Make/MakeController.php
+++ src/Console/Commands/Make/MakeController.php
@@ -9,228 +9,239 @@ use function Laravel\Prompts\confirm;
 
 class MakeController extends ControllerMakeCommand
 {
-	use TraitPackagizer;
-	use TraitCallDelegation;
-	use TraitCreatesMatchingTest;
+    use TraitPackagizer;
+    use TraitCallDelegation;
+    use TraitCreatesMatchingTest;
 
-	/**
-	 * Build the model replacement values.
-	 * Will be called if a model-option was set
-	 *
-	 * @param array $replace
-	 *
-	 * @return array
-	 */
-	protected function buildModelReplacements(array $replace): array
-	{
-		return $this->package() ? $this->buildModelReplacements_package($replace) :
-			parent::buildModelReplacements($replace);
-	}
+    /**
+     * Build the model replacement values.
+     * Will be called if a model-option was set
+     *
+     * @param array $replace
+     *
+     * @return array
+     */
+    protected function buildModelReplacements(array $replace): array
+    {
+        return $this->package() ? $this->buildModelReplacements_package($replace) :
+            parent::buildModelReplacements($replace);
+    }
 
-	/**
-	 * Build the class with the given name.
-	 *
-	 * Remove the base controller import if we are already in the base namespace.
-	 *
-	 * @param string $name
-	 *
-	 * @return string
-	 */
-	protected function buildClass($name)
-	{
-		if (! $this->package()) {
-			return parent::buildClass($name);
-		}
-		// todo if --requests do it into solved in future but not by model
-		// todo in Model handle the --all option without creating controller.
-		// That should modify the --all option model
-		$packageNamespace = $this->package()
-										   ->srcPackageNamespace()
-		;
-		$rootAppNamespace           = $this->rootNamespace();
-		$rootAppControllerNamespace = 'App\Http\Controllers';
+    /**
+     * Build the class with the given name.
+     *
+     * Remove the base controller import if we are already in the base namespace.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        if (!$this->package()) {
+            return parent::buildClass($name);
+        }
+        // todo if --requests do it into solved in future but not by model
+        // todo in Model handle the --all option without creating controller.
+        // That should modify the --all option model
 
-		$replace = [];
+        $replace = [];
 
-		if ($this->option('parent')) {
-			$replace = $this->buildParentReplacements();
-		}
+        if ($this->option('parent')) {
+            $replace = $this->buildParentReplacements();
+        }
 
-		if ($this->option('model')) {
-			// during options there is a --model flag,
-			// the option is a select option
-			// so the model is want but there is no input for the model name or selectables
-			$replace = $this->buildModelReplacements_package($replace);
-		}
+        if ($this->option('model')) {
+            // during options there is a --model flag,
+            // the option is a select option
+            // so the model is want but there is no input for the model name or selectables
+            $replace = $this->buildModelReplacements_package($replace);
+        }
 
-		if ($this->option('creatable')) {
-			$replace['abort(404);'] = '//';
-		}
+        if ($this->option('creatable')) {
+            $replace['abort(404);'] = '//';
+        }
 
-		$baseControllerExists = file_exists($this->getPath("{$packageNamespace}Http\Controllers\Controller"));
+        return $this->classReplacements($replace, $name);
+    }
 
-		if ($baseControllerExists) {
-			$replace["use {$rootAppControllerNamespace}\Controller;\n"] = "use {$packageNamespace}\Http\Controllers\Controller;\n";
-		} else {
-			$replace[' extends Controller']                                    = '';
-			$replace["use {$rootAppNamespace}\Http\Controllers\Controller;\n"] = '';
-		}
+    protected function classReplacements(array $replace, string $name)
+    {
+        $packageNamespace           = $this->package()
+                                           ->srcPackageNamespace()
+        ;
+        $rootAppNamespace           = $this->rootNamespace();
+        $rootAppControllerNamespace = 'App\Http\Controllers';
 
-		return str_replace(
-			array_keys($replace),
-			array_values($replace),
-			// has to be the parent parent class
-			$this->replaceControllerStub($name),
-		);
-	}
+        $baseControllerExists = file_exists($this->getPath("{$packageNamespace}Http\Controllers\Controller"));
 
-	protected function replaceControllerStub(string $name)
-	{
-		$stub = $this->files->get($this->getStub());
+        if ($baseControllerExists) {
+            $replace["use {$rootAppControllerNamespace}\Controller;\n"] =
+                "use {$packageNamespace}\Http\Controllers\Controller;\n";
+        }
+        else {
+            $replace[' extends Controller']                                    = '';
+            $replace["use {$rootAppNamespace}\Http\Controllers\Controller;\n"] = '';
+        }
 
-		return $this->replaceNamespace($stub, $name)
-					->replaceClass($stub, $name)
-		;
-	}
+        return str_replace(
+            array_keys($replace),
+            array_values($replace),
+            // has to be the parent class
+            $this->replaceControllerStub($name),
+        );
+    }
 
-	/**
-	 * @param $replace
-	 *
-	 * @return array
-	 */
-	protected function buildModelReplacements_package($replace): array
-	{
-		$relativeModel = $this->option('model');
+    protected function replaceControllerStub(string $name): string
+    {
+        $stub = $this->files->get($this->getStub());
 
-		// don't know where the lower case results from
-		if (in_array($relativeModel, ['model', true], true)) {
-			// generate Model Class from Controller
-			$relativeModel = Str::replaceEnd('Controller', '', $this->argument('name'));
-			$this->input->setOption('model', $relativeModel);
-		}
+        return $this->replaceNamespace($stub, $name)
+                    ->replaceClass($stub, $name)
+        ;
+    }
 
-		$modelClass = $this->parseModel($relativeModel);
-		$this->makeModelIfNeed($modelClass, $relativeModel);
+    /**
+     * @param $replace
+     *
+     * @return array
+     */
+    protected function buildModelReplacements_package($replace): array
+    {
+        $relativeModel = $this->option('model');
 
-		$replace = $this->buildFormRequestReplacements_package($replace, $modelClass);
+        // don't know where the lower case results from
+        if (in_array($relativeModel, ['model', true], true)) {
+            // generate Model Class from Controller
+            $relativeModel = Str::replaceEnd('Controller', '', $this->argument('name'));
+            $this->input->setOption('model', $relativeModel);
+        }
 
-		return $this->mergeReplacements($replace, $modelClass);
-		// keep sync with ControllerMakeCommand
-	}
+        $modelClass = $this->parseModel($relativeModel);
+        $this->makeModelIfNeed($modelClass, $relativeModel);
 
-	protected function mergeReplacements(array $replace, string $modelClass)
-	{
-		$cbn   = class_basename($modelClass);
-		$cbLcf = lcfirst($cbn);
+        $replace = $this->buildFormRequestReplacements_package($replace, $modelClass);
 
-		// keep sync with ControllerMakeCommand
-		return array_merge(
-			$replace,
-			[
-				'DummyFullModelClass'   => $modelClass,
-				'{{ namespacedModel }}' => $modelClass,
-				'{{namespacedModel}}'   => $modelClass,
-				'DummyModelClass'       => $cbn,
-				'{{ model }}'           => $cbn,
-				'{{model}}'             => $cbn,
-				'DummyModelVariable'    => $cbLcf,
-				'{{ modelVariable }}'   => $cbLcf,
-				'{{modelVariable}}'     => $cbLcf,
-			],
-		);
-	}
+        return $this->mergeReplacements($replace, $modelClass);
+    }
 
-	protected function makeModelIfNeed(string $modelClass, string $relativeModel): void
-	{
-		if (
-			! class_exists($modelClass) && confirm(
-				"A {$modelClass} model does not exist. Do you want to generate it?",
-				true,
-			)
-		) {
-			$this->call('make:model', ['name' => $relativeModel]);
-		}
-	}
+    protected function mergeReplacements(array $replace, string $modelClass)
+    {
+        $cbn   = class_basename($modelClass);
+        $cbLcf = lcfirst($cbn);
 
-	/**
-	 * Build the model replacement values.
-	 *
-	 * @param array  $replace
-	 * @param string $modelClass
-	 *
-	 * @return array
-	 */
-	protected function buildFormRequestReplacements_package(array $replace, string $modelClass): array
-	{
-		[$namespace, $storeRequestClass, $updateRequestClass] = [
-			'Illuminate\\Http',
-			'Request',
-			'Request',
-		];
+        // keep sync with ControllerMakeCommand
+        return array_merge(
+            $replace,
+            [
+                'DummyFullModelClass'   => $modelClass,
+                '{{ namespacedModel }}' => $modelClass,
+                '{{namespacedModel}}'   => $modelClass,
+                'DummyModelClass'       => $cbn,
+                '{{ model }}'           => $cbn,
+                '{{model}}'             => $cbn,
+                'DummyModelVariable'    => $cbLcf,
+                '{{ modelVariable }}'   => $cbLcf,
+                '{{modelVariable}}'     => $cbLcf,
+            ],
+        );
+    }
 
-		if ($this->option('requests')) {
-			$namespace = $this->package()
-							  ->joinPackageNamespace('Http', 'Requests')
-			;
+    protected function makeModelIfNeed(string $modelClass, string $relativeModel): void
+    {
+        if (
+            !class_exists($modelClass) && confirm(
+                "A {$modelClass} model does not exist. Do you want to generate it?",
+                true,
+            )
+        ) {
+            $this->call('make:model', ['name' => $relativeModel]);
+        }
+    }
 
-			[$storeRequestClass, $updateRequestClass] = $this->generateFormRequests(
-				$modelClass,
-				$storeRequestClass,
-				$updateRequestClass,
-			);
-		}
+    /**
+     * Build the model replacement values.
+     *
+     * @param array  $replace
+     * @param string $modelClass
+     *
+     * @return array
+     */
+    protected function buildFormRequestReplacements_package(array $replace, string $modelClass): array
+    {
+        [$namespace, $storeRequestClass, $updateRequestClass] = [
+            'Illuminate\\Http',
+            'Request',
+            'Request',
+        ];
 
-		$namespacedRequests = $namespace.'\\'.$storeRequestClass.';';
+        if ($this->option('requests')) {
+            $namespace = $this->package()
+                              ->joinPackageNamespace('Http', 'Requests')
+            ;
 
-		if ($storeRequestClass !== $updateRequestClass) {
-			$namespacedRequests .= PHP_EOL.'use '.$namespace.'\\'.$updateRequestClass.';';
-		}
+            [$storeRequestClass, $updateRequestClass] = $this->generateFormRequests(
+                $modelClass,
+                $storeRequestClass,
+                $updateRequestClass,
+            );
+        }
 
-		return $this->replaceFormRequests_package([$replace, $namespace, $namespacedRequests, $storeRequestClass, $updateRequestClass]);
-	}
+        $namespacedRequests = $namespace . '\\' . $storeRequestClass . ';';
 
-	protected function replaceFormRequests_package(array $vars): array
-	{
-		[$replace,$namespace, $namespacedRequests,$storeRequestClass,$updateRequestClass] = $vars;
+        if ($storeRequestClass !== $updateRequestClass) {
+            $namespacedRequests .= PHP_EOL . 'use ' . $namespace . '\\' . $updateRequestClass . ';';
+        }
 
-		return array_merge(
-			$replace,
-			[
-				'{{ storeRequest }}'            => $storeRequestClass,
-				'{{storeRequest}}'              => $storeRequestClass,
-				'{{ updateRequest }}'           => $updateRequestClass,
-				'{{updateRequest}}'             => $updateRequestClass,
-				'{{ namespacedStoreRequest }}'  => $namespace.'\\'.$storeRequestClass,
-				'{{namespacedStoreRequest}}'    => $namespace.'\\'.$storeRequestClass,
-				'{{ namespacedUpdateRequest }}' => $namespace.'\\'.$updateRequestClass,
-				'{{namespacedUpdateRequest}}'   => $namespace.'\\'.$updateRequestClass,
-				'{{ namespacedRequests }}'      => $namespacedRequests,
-				'{{namespacedRequests}}'        => $namespacedRequests,
-			],
-		);
-	}
+        return $this->replaceFormRequests_package([$replace,
+                                                   $namespace,
+                                                   $namespacedRequests,
+                                                   $storeRequestClass,
+                                                   $updateRequestClass]);
+    }
 
-	/**
-	 * @param $model
-	 *
-	 * @return string
-	 */
-	protected function parseModel($model): string
-	{
-		return $this->package() ? $this->parseModel_package($model) : parent::parseModel($model);
-	}
+    protected function replaceFormRequests_package(array $vars): array
+    {
+        [$replace, $namespace, $namespacedRequests, $storeRequestClass, $updateRequestClass] = $vars;
 
-	/**
-	 * @param $model
-	 *
-	 * @return string
-	 */
-	protected function parseModel_package($model): string
-	{
-		if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
-			throw new InvalidArgumentException('Model name contains invalid characters.');
-		}
+        return array_merge(
+            $replace,
+            [
+                '{{ storeRequest }}'            => $storeRequestClass,
+                '{{storeRequest}}'              => $storeRequestClass,
+                '{{ updateRequest }}'           => $updateRequestClass,
+                '{{updateRequest}}'             => $updateRequestClass,
+                '{{ namespacedStoreRequest }}'  => $namespace . '\\' . $storeRequestClass,
+                '{{namespacedStoreRequest}}'    => $namespace . '\\' . $storeRequestClass,
+                '{{ namespacedUpdateRequest }}' => $namespace . '\\' . $updateRequestClass,
+                '{{namespacedUpdateRequest}}'   => $namespace . '\\' . $updateRequestClass,
+                '{{ namespacedRequests }}'      => $namespacedRequests,
+                '{{namespacedRequests}}'        => $namespacedRequests,
+            ],
+        );
+    }
 
-		return $this->qualifyModel($model);
-	}
+    /**
+     * @param $model
+     *
+     * @return string
+     */
+    protected function parseModel($model): string
+    {
+        return $this->package() ? $this->parseModel_package($model) : parent::parseModel($model);
+    }
+
+    /**
+     * @param $model
+     *
+     * @return string
+     */
+    protected function parseModel_package($model): string
+    {
+        if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
+            throw new InvalidArgumentException('Model name contains invalid characters.');
+        }
+
+        return $this->qualifyModel($model);
+    }
 }
