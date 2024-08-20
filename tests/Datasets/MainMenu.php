40c85eacc3ac2aea2618355d5d74e73435f08515
RT Filefabrik <rt@filefabriik.com>
2024-08-20 18:58:18 +0200

    kosmetik v1.0.7

--- tests/Datasets/MainMenu.php
+++ tests/Datasets/MainMenu.php
@@ -8,7 +8,7 @@ dataset(
 	[
 		['label' => 'add components', 'method' => 'handle_package'],
 		['label' => 'new laravel composer package', 'method' => 'create_package'],
-		['label' => 'list Packages', 'method' => 'list_packages'],
+		['label' => 'list packages', 'method' => 'list_packages'],
 		// Handle composer Package into the Laravel-Host
 		['label' => 'add VendorPackage with there Repository (Laravel Host Composer )', 'method' => 'composer_add_repository_vendor_package'],
 		['label' => 'remove VendorPackage with there Repository (Laravel Host Composer)', 'method' => 'composer_remove_repository_vendor_package'],
